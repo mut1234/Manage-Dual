d48bdb225b68feafa8520eb3a8760c0ac8985fa4
Mustafa Ghnaim <44782684+mut1234@users.noreply.github.com>
2023-09-28 18:20:20 +0300

    Add Search, Sort and Pagination to Rest Api , Fix remove ,updated

--- manage dual/Controllers/MangeDualApiController.cs
+++ manage dual/Controllers/MangeDualApiController.cs
@@ -3,6 +3,8 @@ using manage_dual.Models;
 using manage_dual.NewFolder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
 
 namespace manage_dual.Controllers
 {
@@ -76,6 +78,7 @@ namespace manage_dual.Controllers
 
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpDelete("{id:int}", Name = "DeleteClient")]
         public async Task<IActionResult> DeleteClient(int id)//IActionResult you can not return with it
@@ -90,21 +93,24 @@ namespace manage_dual.Controllers
                 _db.client.Remove(result);
                 await _db.SaveChangesAsync();
             }
-            return NoContent();
-
+            return Ok();
         }
         [HttpPut("{id:int}", Name = "UpdateClient")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
         public async Task<IActionResult> UpdateClient(int id, [FromBody] client client)
         {
-            if (client == null || id != client.clientId)
+            try
+            {
+            var result = await _db.client.FirstOrDefaultAsync(u => u.clientId == id);
+            if (result == null)
             {
                 return BadRequest();
             }
-            var result = await _db.client.FirstOrDefaultAsync(u => u.clientId == id);
             result.Name = client.Name;
-            result.clientId = client.clientId;
+            result.clientId = id;
             result.Payment = client.Payment;
             result.City = client.City;
             result.Item = client.Item;
@@ -113,10 +119,96 @@ namespace manage_dual.Controllers
             result.PostalCode = client.PostalCode;
             result.ClientDateAddedToSystem = client.ClientDateAddedToSystem;
             result.PhoneNumber = client.PhoneNumber;
+            await _db.SaveChangesAsync();
+            return Ok();
 
-            return NoContent();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
+        [HttpGet("Search/{name}")]
+        public async Task<ActionResult<client>> Search(string name)
+        {
+            try
+            {
+                var result = await _db.client.FirstOrDefaultAsync(u => u.Name == name);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                if (result == null)
+                    throw new Exception("The Search result not found.");
+
+                return NotFound();
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        [HttpPost("Sort")]
+        public async Task<ActionResult<IEnumerable<client>>> Sort()
+        {
+            try
+            {
+                var result = await _db.client.ToListAsync();
+
+                if (result != null)
+                {
+                    return Ok(result.OrderByDescending(u => u.Name));
+                }
+                if (result == null)
+                    throw new Exception("The Search result not found.");
+
+                return NotFound();
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+
+        [HttpGet("Pagination/{pageSize}")]
+        public async Task<ActionResult<IEnumerable<client>>> Pagination(int pageSize)
+            {
+            try
+            {
+                var result = await _db.client.ToListAsync();
+                int totalRecords = result.Count();
+                //int pageSize = 3;
+                int currentPage = 1;
+                if (totalRecords > 0 && result != null)
+                {
+                    int totalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+
+                    result = result.Skip(currentPage).Take(totalPages).ToList();
+                    return Ok(result);
+
+                }
+
+                if (result == null)
+                    throw new Exception("The Search result not found.");
+
+                return NotFound();
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
     }
 
 
---
+++ manage dual/Migrations/20230928125729_t1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace manage_dual.Migrations
+{
+    /// <inheritdoc />
+    public partial class t1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
